6ada2c2645410b4d7a31f8fa72bfc4bb1c98eb44
jomlink <jonasmwansa7@gmail.com>
2022-06-30 14:54:51 -0700

    using printf method documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,14 +2,14 @@
 /**
  *	main - entry point
  *
- * 	printf - prints to screen the stream of characters
+ * 	printf - prints "with proper grammar, but the outcome is a piece of art"
  *
- *	Return : always 0 (success)
+ *	Return: Always 0 (Success)
  *
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
