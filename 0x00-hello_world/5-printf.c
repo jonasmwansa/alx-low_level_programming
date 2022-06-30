9689ca55a5099fd817924cabc14b924f1b54ee07
jomlink <jonasmwansa7@gmail.com>
2022-06-30 14:38:59 -0700

    using printf method

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *	main - entry point
+ *
+ * 	printf - prints to screen the stream of characters
+ *
+ *	Return - always 0 for success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
