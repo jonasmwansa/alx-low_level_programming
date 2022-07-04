db6b6896591fa0c7a518760d64d508f0d5ac7cd0
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-05 01:10:03 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,10 +12,14 @@ int main(void)
 	char low;
 
 	for (d = '0'; d <= '9'; d++)
+	{
 		putchar(d);
+	}	
 	for (low = 'a'; low <= 'f'; low++)
+	{
 		putchar(low);
-		putchar('\n');
+	}	
+ 	putchar('\n');
  	
  	return (0);
 }
