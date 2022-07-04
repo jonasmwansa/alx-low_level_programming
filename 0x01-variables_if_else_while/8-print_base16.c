16986c11d8ba3e6362238130309760e5b620a613
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-05 01:07:53 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,39 +8,14 @@
 *
 */
 int main(void)
-{
-	int num;
+{	int d;
+	char low;
 
-	for (num = 0; num < 16; num++)
-	{	
-		if(num == 10)
-		{
-			num="a";
-		}
-		else if(num == 11)
-		{
-			num = "b";
-		}
-		else if(num == 12)
-		{
-			num = 'c';
-		}
-		else if(num == 13)
-		{
-			num = 'd';
-		}
-		else if(num == 14)
-		{
-			num = 'e';
-		}
-		else if(num == 15)
-		{
-			num = 'f';
-		}else{
-			printf("%d", num);
-		}
-	}
-	printf("\n");
-
-	return (0);
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+		putchar('\n');
+ 	
+ 	return (0);
 }
