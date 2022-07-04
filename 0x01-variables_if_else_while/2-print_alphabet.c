c71635dbf62f3500dd48a9490e3117b92df79f4f
jomlink <jonasmwansa7@gmail.com>
2022-07-04 14:12:01 -0700

    commit to print alphabetical order

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point for program
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char alphabet[]="abcdefghijklmnopqrstuvwxyz";
+	printf(alphabet);
+	
+	return (0);
+}
