36d6954964ba062883938b83d271b0898f2f8160
jomlink <jonasmwansa7@gmail.com>
2022-07-04 14:33:59 -0700

    to upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point for program
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	char upperAlpha = toupper(alphabet);
+	
+	printf("%s%s\n", alphabet), upperAlpha):
+	return (0);
+}
