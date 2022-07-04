1e195abde8283512c814603984be22b3c9cf773b
jomlink <jonasmwansa7@gmail.com>
2022-07-04 15:41:09 -0700

    numbers less than 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point for program
+* Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '10'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	
+	return (0);
+}
