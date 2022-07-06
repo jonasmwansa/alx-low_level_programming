ea88230ec5c7cca67e9547d8a9082020173a888c
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-06 22:53:08 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
+/**
+* print_alphabet - prints all aphabets in lowercase
+*
+* Description: They are sorted by default
+* return (0)
+*/
 void print_alphabet(void)
 {
 	char i;
-
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
-		
 	}
 	_putchar('\n');
 
