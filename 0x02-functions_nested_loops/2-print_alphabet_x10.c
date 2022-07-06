e1fa477fcc3da08ba62b73627466620d45d303eb
jomlink <jonasmwansa7@gmail.com>
2022-07-06 14:20:38 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ *  print_alphabet_x10 - prints alphabets 10
+ *
+ *   
+ *
+ *  Description: prints 10 times
+ *
+ *   Return: Always(0).
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	
+	while (count < 10)
+	{
+		char i;
+			
+		for ( i = 'a'; i < 'z'; i++)
+		{
+			_putchar(ch);
+		}
+	count++;
+	_putchar('\n');
+	}
+}
