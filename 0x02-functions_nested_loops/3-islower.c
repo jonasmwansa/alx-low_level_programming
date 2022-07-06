f9538fd992b960d07590a350a82a7a192f18a044
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 00:39:27 +0200

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,11 +10,7 @@ int main(void)
 {
 	int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
+	r = _islower('c');
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
