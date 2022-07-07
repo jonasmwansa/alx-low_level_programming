c88fa7cf2c6006881f10c2507091ea5a951cdf8a
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 18:31:30 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,24 +7,15 @@
 *  Return: 0
 *
 */
-int main(void)
-{
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
 	return (0);
 }
