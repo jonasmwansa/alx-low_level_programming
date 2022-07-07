723869d92b79876053b3893b50b62ef6ac5fd0fe
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 02:43:42 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,21 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
+		{
 			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
 			n--;
+		}
 	}
 	else
 	{
 		while (n < 98)
+		{
 			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
 			n++;
+		}
 	}
 }
