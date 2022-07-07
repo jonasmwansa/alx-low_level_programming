c8cc980a38c165771846b8ae566fc97eb0e46744
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 02:35:00 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,90 @@
 #include "main.h"
-
 /**
-* main - Entry point for program
-* Return: Always 0 (Success)
-*
-*/
+ * print_to_98 - prints numbers to 98
+ * @n: first parameter
+ *
+ * Description: prints all num to 98
+ * Return: Always (0).
+ */
 
-int main(void)
+void print_to_98(int n)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	int end;
 
+	end = 98;
+
+	if (n < end)
+	{
+		for (; n <= end; n++)
+		{
+			if (n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				break;
+			}
+			else
+			{
+				if (n >= -9 && n <= 9)
+				{
+					if (n < 0)
+					{
+						_putchar('-');
+						_putchar((n * -1) + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+					else if (n >= 0)
+					{
+						_putchar(n + '0');
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else if (n >= 100)
+				{
+					_putchar((n / 100) + '0');
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	else if (n == 0)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
+	}
+	else
+	{
+		for (; n >= end; n--)
+		{
+			if (n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				break;
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	return;
 }
