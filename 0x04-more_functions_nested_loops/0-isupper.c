597c938c0758d6f23aa46f444fa6035c20023fca
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 23:42:54 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 
 /**
-* _isupper - prints 1 or 0 depending on input
-* @c: input type variable
+* main - check the code.
 *
-* Description: checks for uppercase letters
-* Return: Aways (0).
+* Return: Always 0.
 */
 
-int _isupper(int c)
+int main(void)
 {
-	return (c >= 'A' && c <= 'Z');
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
