40cf3e7e5a4bc95ccba4ef92d387c632e5e9340e
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-07 21:48:47 +0200

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,21 +1,36 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-*
-*  main - tests function that prints if integer is positive or negative
-*  positive_or_negative - function returns if integer is positive or negative
-*  i@ : first parameter
-*  Return: 0
-*
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
 */
 
-int main(void)
+void print_remaining_days(int month, int day, int year)
 {
-	int i;
-
-	i = 98;
-
-	positive_or_negative(i);
-
-	return (0);
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
+		if (month >= 3 && day >= 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
