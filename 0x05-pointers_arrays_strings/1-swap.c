a478dab2fb8687d21b1e26edaf39eaef7c358175
jomlink <jonasmwansa7@gmail.com>
2022-07-11 16:00:57 -0700

    swap numbers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: pointer to int n
+ * Return: Always 0.
+ */
+
+void void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
