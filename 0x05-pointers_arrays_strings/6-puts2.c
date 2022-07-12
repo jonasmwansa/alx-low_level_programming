6c2897e4d8373fbf205df8255c767cdac6e60da7
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-12 13:00:48 +0200

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		putchar(*(str + i));
-		i = i + 1;
+		putchar(str[i]);	
+		i++;
 	}
 	putchar('\n');
 }
