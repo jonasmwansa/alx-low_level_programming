24eede3406ab6425651516c423358730d3206ba2
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-12 03:00:50 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,13 +2,40 @@
 #include <stdio.h>
 
 /**
-* swap_int - takes two integer values and swap them
-* @a: pointer to first value
-* @b: pointer to second value
+* puts_half - print half
+* @str: input string
+*
+* Description: prints half of a string
+* Return: Always (0);
 */
 
 void puts_half(char *str)
 {
-	int len = (strlen(str) - 1) / 2 ;
+	int i, len;
 
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		len = (i + 1) / 2;
+
+		while (str[len] != '\0')
+		{
+			_putchar(str[len]);
+			len++;
+		}
+	}
+	else
+	{
+		len= (i / 2);
+
+		while (str[len] != '\0')
+		{
+			_putchar(str[len]);
+			len++;
+		}
+	}
+	_putchar('\n');
 }
