674a21d37204328b83f6f641b6be278811b887ca
jomlink <jonasmwansa7@gmail.com>
2022-07-11 18:09:15 -0700

    print an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - takes two integer values and swap them
+ * @a: pointer to first value
+ * @b: pointer to second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
