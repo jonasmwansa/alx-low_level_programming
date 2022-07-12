350e087723f6bae5e97103f9691870393e5c3176
jomlink <jonasmwansa7@gmail.com>
2022-07-11 17:56:22 -0700

    printing half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* swap_int - takes two integer values and swap them
+* @a: pointer to first value
+* @b: pointer to second value
+*/
+
+void puts_half(char *str)
+{
+	int len = (strlen(str) - 1) / 2 ;
+
+}
