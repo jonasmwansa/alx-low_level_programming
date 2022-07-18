191be08a0db61e4162ef8e3c889b1b9eb65171fb
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-19 01:14:13 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * _memset - fills string with constant char b upto n bytes
+ * @s: input pointer to string
+ * @b: constant char
+ * @n: number of bytes
+ * Return: pointer to s string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j = 0;
+
+	while (j < n)
+	{
+		s[j] = b;
+		j++;
+	}
+	return (s);
+}
