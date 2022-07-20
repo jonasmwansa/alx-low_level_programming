d43dd9f661a450964decfcb3aa7b9dfe496c0b5d
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-21 01:35:03 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-* _puts_recursion - print a string.
-* @s:pointer to char
-* Return:nothing.
-*/
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-void _puts_recursion(char *s)
+int prime2(int a, int b)
 {
-	if (*s != '\0')
-	{
-		putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		putchar('\n');
-	}
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
