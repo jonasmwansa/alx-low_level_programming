0d14682a12e8d3e2349795b9cd335d8ba18c2a53
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-26 22:24:42 +0200

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,27 +3,51 @@
 #include <stdio.h>
 
 /**
-* create_array - creates an array of chars,
-* and initializes it with a specific char
-* @size: input size
-* @c: input character
-*
-* Return: a pointer to the array, or NULL if it fails
-*/
+ * str_concat - concatenates two strings
+ * @s1: first input string
+ * @s2: second input string
+ *
+ * Return: returns NULL on failure
+ */
 
-char *create_array(unsigned int size, char c)
+char *str_concat(char *s1, char *s2)
 {
-	unsigned int i = 0;
-	char *arr;
+	int s_len1, s_len2, i, j;
+	char *str;
 
-	arr = (char *) malloc(size);
-	if (size == 0 || arr == NULL)
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	else if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (s_len1 = 0; s1[s_len1] != '\0'; s_len1++)
+		continue;
+	for (s_len2 = 0; s2[s_len2] != '\0'; s_len2++)
+		continue;
+	str = malloc(s_len1 + s_len2 + 1);
+
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	i = 0;
+
+	while (i < s_len1)
 	{
-		arr[i] = c;
+		str[i] = s1[i];
+		i++;
 	}
-	return (arr);
+	j = 0;
+
+	while (i < (s_len1 + s_len2))
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
 }
