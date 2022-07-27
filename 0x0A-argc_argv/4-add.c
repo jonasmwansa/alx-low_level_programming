973b9a40e0b208e811c243a319062b2dfd67235d
jomlink <jonasmwansa7@gmail.com>
2022-07-27 14:59:47 -0700

    program that adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the function.
+ * @argc: counts agruments passed to the function for int.
+ * @argv: gives the value passed to the function for char.
+ * Return: Returns always success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 1;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	}
+	return (0);
+}
