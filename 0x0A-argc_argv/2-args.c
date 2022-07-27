cd218437b0fb2f0c836ae66e31411dfe1da4ec32
Jonas Mwansa <jonasmwansa7@gmail.com>
2022-07-27 18:27:08 +0200

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the content of the program.
- * @argc: is a counter of argument for int.
- * @argv: is a counter for value of the argument to char
- * Return: Returns always success.
- */
+* main - prints the function.
+* @argc: is argument counter to an int.
+* @argv: is an agument value determiner for char.
+* Return: Returns always success.
+*/
 
 int main(int argc, char *argv[])
 {
-	(void)argv;
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
-	printf("%d\n", (argc - 1));
-
-	return (0);
+return (0);
 }
